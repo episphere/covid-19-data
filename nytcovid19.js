79b288d21f2fac36d39eae9d68aaacc7e62b9df7
Jonas Almeida <jalmeida@mathbiol.org>
2020-03-29 20:26:45 -0400

    allowing counties from no known state
    such as "Northern Mariana Islands"

--- nytcovid19.js
+++ nytcovid19.js
@@ -30,22 +30,47 @@ nytcovid19.getAllData=async(urlStates=nytcovid19.urlStates,urlCounties=nytcovid1
     let arr =txt.split(/[\n\r]+/).map(r=>r.split(','))
     let labels=arr[0]
     arr.slice(1).forEach(r=>{
-        if(!dt[r[2]].counties){
-            dt[r[2]].counties={}
-        }
-        dt[r[2]].counties[r[1]]={}
-        dt[r[2]].counties[r[1]][labels[0]]=new Date(r[0]) //date
-        dt[r[2]].counties[r[1]][labels[3]]=parseFloat(r[3]) // fips
-        dt[r[2]].counties[r[1]][labels[4]]=parseFloat(r[4]) // cases
-        dt[r[2]].counties[r[1]][labels[5]]=parseFloat(r[5]) // deaths
-        let fipsi = fips[dt[r[2]].counties[r[1]][labels[3]]=parseFloat(r[3])]
-        if(fipsi){ // if there is fips geo coordinates in Josh's file for this fips location
-            dt[r[2]].counties[r[1]].lat=fipsi.lat,
-            dt[r[2]].counties[r[1]].long=fipsi.long
-        }
-
+        if(dt[r[2]]){
+            if(!dt[r[2]].counties){
+                dt[r[2]].counties={}
+            }
+            dt[r[2]].counties[r[1]]={}
+            dt[r[2]].counties[r[1]][labels[0]]=new Date(r[0]) //date
+            dt[r[2]].counties[r[1]][labels[3]]=parseFloat(r[3]) // fips
+            dt[r[2]].counties[r[1]][labels[4]]=parseFloat(r[4]) // cases
+            dt[r[2]].counties[r[1]][labels[5]]=parseFloat(r[5]) // deaths
+            let fipsi = fips[dt[r[2]].counties[r[1]][labels[3]]=parseFloat(r[3])]
+            if(fipsi){ // if there is fips geo coordinates in Josh's file for this fips location
+                dt[r[2]].counties[r[1]].lat=fipsi.lat,
+                dt[r[2]].counties[r[1]].long=fipsi.long
+            }
+        }else{
+            // i.e. "Northern Mariana Islands" assigned to "Unknown" state
+            // debugger 
+        }   
     })
     return dt
+}
+
+//nytcovid19.getScript = async function(url){await import(url)}
+
+nytcovid19.getScript = async function(url){
+    return new Promise(function(resolve,reject){
+        let s = document.createElement('script')
+        s.src=url
+        s.onload=_=>{
+            setTimeout(_=>{delete s},1000)
+            resolve(`${url} loaded`)
+        }
+        s.onerror=_=>reject(Error(`error loading ${url}`))
+        document.head.appendChild(s)
+    })
+}
+
+nytcovid19.plotly=async function(){
+    if(typeof(Plotly)=='undefined'){
+        await nytcovid19.getScript('https://cdn.plot.ly/plotly-latest.min.js')
+    }
 }
 
 if(typeof(define)!='undefined'){
